21c5b6cefd82ff638cc89dc3b4d09f1fb0d1c03e
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2025-07-15 21:28:37 +0600

    test

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define nl '\n'
+#define ll long long
+#define all(c) c.begin(),c.end()
+#define print(c) for(auto e : c) cout << e << " "; cout << nl
+using namespace std;
+void solve()
+{
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t; cin >> t;
+    for(int tt = 1; tt <= t; tt++)
+    {
+        // cout << "TEST CASE-" << tt << nl;
+        solve();
+    }
+
+    return 0;
+}
