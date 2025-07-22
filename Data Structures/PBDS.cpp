92b891de0ff366408c05015901a2a7f1c33ee7e5
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2025-07-23 01:32:18 +0600

    added footnote

--- Data Structures/PBDS.cpp
+++ Data Structures/PBDS.cpp
@@ -8,13 +8,6 @@
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-/*
-if (x >= 0 && x < (int)o_mulset.size()) // → k-th smallest element (0-based)
-{
-   cout << *o_mulset.find_by_order(x) << nl; 
-} 
-cout << o_mulset.order_of_key(x) << nl; // → count of elements strictly < x
-*/
 using namespace std;
 
 template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // less_equal for multiset
@@ -44,6 +37,25 @@ int main()
     }
     
     cout << cnt << nl;
+    
 
     return 0;
 }
+/* Basic Operations
+if (x >= 0 && x < (int)o_mulset.size()) // → k-th smallest element (0-based)
+{
+   cout << *o_mulset.find_by_order(x) << nl; 
+} 
+cout << o_mulset.order_of_key(x) << nl; // → count of elements strictly < x
+*/
+/* Useful Technique
+Use pbds<pair<int, int>> to store {value, index} pairs.
+Since ordered_set (PBDS) doesn’t allow duplicates, the index makes each entry unique.
+
+    // Get values by index, but sorted based on their values.***
+    window.insert({VALUE, index});          // insert value with index
+    auto it = window.find_by_order(idx);    // get idx-th smallest element
+    cout << it->first << " ";               // access the VALUE
+
+    window.erase({VALUE, index});           // remove VALUE at index
+*/
