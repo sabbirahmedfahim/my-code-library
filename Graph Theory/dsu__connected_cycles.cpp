7f72999281b674c76c4fa1b56976af262aabe589
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2025-08-01 01:35:39 +0600

    Union by size added, code length reduced

--- Graph Theory/dsu__connected_cycles.cpp
+++ Graph Theory/dsu__connected_cycles.cpp
@@ -4,43 +4,42 @@
 #define nl '\n'
 using namespace std;
 const int N = 2e5 + 5;
-int parent[N];
+int parent[N], group_size[N];
 vector<int> adj[N];
-void dsu_initialize(int n)
+void dsu_initialize(int n) 
 {
-    for (int i = 0; i <= n; i++)
-    {
-        parent[i] = -1;
-    }
+    for (int i = 0; i <= n; i++) parent[i] = -1, group_size[i] = 1;
 }
-int dsu_find(int node)
+int dsu_find(int node) 
 {
     if(parent[node] == -1) return node;
-    int leader = dsu_find(parent[node]);
-    parent[node] = leader; // path compression
-    return leader;
+    return parent[node] = dsu_find(parent[node]); // path compression
 }
+
 void dsu_union(int node1, int node2) 
 {
-    int leaderA = dsu_find(node1); 
-    int leaderB = dsu_find(node2); 
-    if(leaderA != leaderB) parent[leaderA] = leaderB; 
-}
-int main()
-{
-    int n, e; cin >> n >> e;
-    dsu_initialize(n); 
-    
-    int cntCycle = 0;
-    while (e--)
+    int leaderA = dsu_find(node1), leaderB = dsu_find(node2);
+    if(leaderA != leaderB) 
     {
-        int u, v; cin >> u >> v;
+        if(group_size[leaderA] < group_size[leaderB]) 
+            swap(leaderA, leaderB);
+        parent[leaderB] = leaderA;
+        group_size[leaderA] += group_size[leaderB];
+    }
+}
 
+int main() {
+    int n, e; cin >> n >> e;
+    dsu_initialize(n);
+
+    int cntCycle = 0;
+    while (e--) {
+        int u, v; cin >> u >> v;
         dsu_union(u, v);
-        
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
+
 
     map<int, vector<int> > leaders;
     for (int i = 1; i <= n; i++)
@@ -63,8 +62,7 @@ int main()
 
         if(isCycle) cntCycle++;
     }
-    
-    cout << cntCycle << nl; 
 
+    cout << cntCycle << nl;
     return 0;
 }
--- Graph Theory/dsu__kruskal_mst.cpp
+++ Graph Theory/dsu__kruskal_mst.cpp
@@ -1,85 +1,62 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 #define ll long long
 #define nl '\n'
 using namespace std;
 const int N = 1e5 + 5;
-int parent[N];
-int group_size[N]; // union by size/rank in faster than normal union
-void dsu_initialize(int n)
+int parent[N], group_size[N]; // union by size/rank is faster than normal union
+
+void dsu_initialize(int n) 
 {
-    for (int i = 0; i < n; i++)
-    {
-        parent[i] = -1;
-        group_size[i] = 1;
-    }
+    for (int i = 0; i < n; i++) parent[i] = -1, group_size[i] = 1;
 }
-int dsu_find(int node)
+
+int dsu_find(int node) 
 {
     if(parent[node] == -1) return node;
-    int leader = dsu_find(parent[node]);
-    parent[node] = leader; // path compression
-    return leader;
+    return parent[node] = dsu_find(parent[node]); // path compression
 }
-void dsu_union_by_size(int node1, int node2)
+
+void dsu_union_by_size(int node1, int node2) 
 {
-    int leaderA = dsu_find(node1);
-    int leaderB = dsu_find(node2);
+    int leaderA = dsu_find(node1), leaderB = dsu_find(node2);
     if(leaderA == leaderB) return;
-
     if(group_size[leaderA] > group_size[leaderB])
-    {
-        parent[leaderB] = leaderA;
-        group_size[leaderA] += group_size[leaderB]; 
-    }
+        parent[leaderB] = leaderA, group_size[leaderA] += group_size[leaderB]; 
     else
-    {
-        parent[leaderA] = leaderB;
-        group_size[leaderB] += group_size[leaderA];
-    } 
+        parent[leaderA] = leaderB, group_size[leaderB] += group_size[leaderA];
 }
 
-class Edge
+class Edge 
 {
 public:
     int u, v, w; 
-    Edge(int u, int v, int w)
-    {
-        this->u = u;
-        this->v = v;
-        this->w = w;
-    }
+    Edge(int u, int v, int w): u(u), v(v), w(w) {}
 };
-bool cmp(Edge a, Edge b)
+
+bool cmp(Edge a, Edge b) { return a.w < b.w; }
+
+int main() 
 {
-    return a.w < b.w;
-}
-int main()
-{
-    int n, e;
-    cin >> n >> e;
+    int n, e; cin >> n >> e;
     dsu_initialize(n); 
     vector<Edge> edgeList;
-    while (e--)
+    while (e--) 
     {
         int u, v, w;
         cin >> u >> v >> w;
-        edgeList.push_back((Edge(u, v, w)));
+        edgeList.push_back(Edge(u, v, w));
     }
     sort(edgeList.begin(), edgeList.end(), cmp); 
     int totalCost = 0; // store costs
-    for (Edge ed : edgeList)
+    for (Edge ed : edgeList) 
     {
-        int leaderU = dsu_find(ed.u);
-        int leaderV = dsu_find(ed.v);
-        if(leaderU == leaderV) continue;
-        else
-        {
+        int leaderU = dsu_find(ed.u), leaderV = dsu_find(ed.v);
+        if(leaderU != leaderV) {
             dsu_union_by_size(ed.u, ed.v);
             totalCost += ed.w;
         } 
     }
     cout << totalCost;
-
     return 0;
 }
 /*
@@ -95,5 +72,4 @@ Input:
 
 Expected Output:
 12
-
 */
