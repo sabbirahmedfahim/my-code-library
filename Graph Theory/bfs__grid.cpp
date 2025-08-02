71d12157fef3734897783421ec5d8e22ed060003
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2025-08-02 16:35:03 +0600

    bfs: better example added

--- Graph Theory/bfs__grid.cpp
+++ Graph Theory/bfs__grid.cpp
@@ -1,4 +1,4 @@
-// https://cses.fi/problemset/task/1192/
+// https://codeforces.com/gym/105819/problem/C
 #include <bits/stdc++.h>
 #define nl '\n'
 #define ll long long
@@ -6,20 +6,27 @@
 #define print(c) for(auto e : c) cout << e << " "; cout << nl
 using namespace std;
 const int N = 1E3 + 5;
-char mat[N][N]; // change data type ***
+int mat[N][N]; // adjust data type ***
 int n, m;
 bool vis[N][N];
-int dis[N][N];
+int dist[N][N];
 vector<pair<int, int> > d = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 bool is_valid(int ci, int cj)
 {
     return (ci < n && ci >= 0 && cj < m && cj >= 0);
 }
-void bfs(int si, int sj)
+int cnt = 0;
+bool cmp(tuple<int, int, int> x, tuple<int, int, int> y)
 {
+    return get<0> (x) > get<0> (y);
+}
+void bfs(int si, int sj) 
+{
+    cnt++;
     queue<pair<int, int> > q;
     q.push({si, sj});
     vis[si][sj] = true;
+    // dist[si][sj] = 0;
     while (!q.empty())
     {
         pair<int,int> par = q.front();
@@ -28,10 +35,12 @@ void bfs(int si, int sj)
         {
             int ci = par.first + d[i].first;
             int cj = par.second + d[i].second;
-            if(is_valid(ci, cj) && !vis[ci][cj] && mat[ci][cj] == '.')
+            // careful with mixed-type comparisons (e.g., char vs int)
+            if(is_valid(ci, cj) && !vis[ci][cj] && mat[ci][cj] < mat[par.first][par.second]) 
             {
                 q.push({ci, cj});
                 vis[ci][cj] = true;
+                // dist[ci][cj] = dist[par.first][par.second] + 1;
             }
         }
     }
@@ -41,27 +50,57 @@ int main()
     ios_base::sync_with_stdio(false); cin.tie(NULL);
  
     cin >> n >> m;
+
+    vector<tuple<int, int, int>> val_idx;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             cin >> mat[i][j];
+            val_idx.push_back({mat[i][j], i, j});
         }
     }
+
+    int res = 0;
+
     memset(vis, false, sizeof(vis));
-    memset(dis, -1, sizeof(dis));
-    int cnt = 0;
-    for (int i = 0; i < n; i++)
+    // memset(dist, -1, sizeof(dist));
+
+    sort(all(val_idx), cmp);
+    for (int i = 0; i < val_idx.size(); i++)
     {
-        for (int j = 0; j < m; j++)
-        {
-            if(mat[i][j] == '.' && !vis[i][j])
-            {
-                cnt++; bfs(i, j);
-            }
-        }
+        auto [data, x, y] = val_idx[i];
+        if(!vis[x][y]) bfs(x, y);
     }
-    cout << cnt << nl;
+
+    res = cnt; cnt = 0;
+
+    memset(vis, false, sizeof(vis));
+    // memset(dist, -1, sizeof(dist));
+
+    reverse(all(val_idx));
+    for (int i = 0; i < val_idx.size(); i++)
+    {
+        auto [data, x, y] = val_idx[i];
+        if(!vis[x][y]) bfs(x, y);
+    }
+
+    res = min(res, cnt);
+
+    cout << res << nl;
+
+    // int cnt = 0;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         if(mat[i][j] == '.' && !vis[i][j])
+    //         {
+    //             cnt++; bfs(i, j);
+    //         }
+    //     }
+    // }
+    // cout << cnt << nl;
  
     return 0;
 }
