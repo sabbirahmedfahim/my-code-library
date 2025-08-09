9e22e6f40907020e76f7885e3c83a9d87dc289c4
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2025-08-09 22:07:51 +0600

    format code

--- Math and Number Theory/Binary_Exponentiation.cpp
+++ Math and Number Theory/Binary_Exponentiation.cpp
@@ -6,28 +6,19 @@
 using namespace std;
 const int MOD = 1e9 + 7;
 
+// mod + normal both 
 ll Pow(ll a, ll b) // O(log b)
-{
-    ll ans = 1 % MOD;
-    a %= MOD;
-    if (a < 0) a += MOD;
-    while (b) 
-    {
-        if (b & 1) ans = (ans * a) % MOD;
-        a = (a * a) % MOD;
-        b >>= 1;
-    }
-    return ans;
-}
-
-// faster and more accurate than built-in pow() for integers
-ll powInt(ll a, ll b) // O(log b)
-{
+{ 
+    // a %= MOD;
+    // if (a < 0) a += MOD;
     ll ans = 1;
-    while (b) 
-    {
-        if (b & 1) ans *= a;
-        a *= a;
+    while (b) {
+        if (b & 1) {
+            ans = ans * a;
+            // ans = (ans * a) % MOD;
+        }
+        a = a * a;
+        // a = (a * a) % MOD;
         b >>= 1;
     }
     return ans;
